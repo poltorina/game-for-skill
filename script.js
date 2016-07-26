8ab21c522bac111185ed259e06b78988370cbb05
Alexandra Poltorina <poltorina96@gmail.com>
2016-07-26 13:45:47 +0300

    without jq

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
+window.onload = zoomDiv;
+
+window.addEventListener('resize', function(){
+	return zoomDiv();
+}, true);
+
 function zoomDiv(){
 
-	var wrapWidth = $('.wrapper').width();
-	var wrapHeight = $('.wrapper').height();
+	var wrapWidth = document.getElementsByTagName('div')[0].clientWidth - 10;
+	var wrapHeight = document.getElementsByTagName('div')[0].clientHeight - 20;
+	var game = document.getElementById('scalable');
+
 	var zoom = wrapWidth / 1518;
 	var maxW = 1518 * zoom - 1;
 	var maxH = 1143 * zoom + 1;
-
-		$('.game').css({
-			'transform': 'scale(' + zoom + ')',
-			'-webkit-transform': 'scale(' + zoom + ')',
-			'-moz-transform': 'scale(' + zoom + ')',
-			'-ms-transform': 'scale(' + zoom + ')',
-			'top': 'calc(50% - ' + maxH / 2 + 'px)',
-			'left': 'calc(50% - ' + maxW / 2 + 'px)'
-		});
+	var scale = 'scale(' + zoom + ')';
 
 	if (wrapHeight < maxH) {
 		var correctHeight = wrapHeight / 1143;
 		var maxH = 1143 * correctHeight + 1;
 		var maxW = 1518 * correctHeight;
-		$('.game').css({
-			'transform': 'scale(' + correctHeight + ')',
-			'-webkit-transform': 'scale(' + correctHeight + ')',
-			'-moz-transform': 'scale(' + correctHeight + ')',
-			'-ms-transform': 'scale(' + correctHeight + ')',
-			'top': 'calc(50% - ' + maxH / 2 + 'px)',
-			'left': 'calc(50% - ' + maxW / 2 + 'px)'
-		});
+		var scale = 'scale(' + correctHeight + ')';
 	}
+
+	function setVendor(element, property, value) {
+	console.log(maxH, maxW);
+
+		element.style["webkit" + property] = value;
+		element.style["moz" + property] = value;
+		element.style["ms" + property] = value;
+		element.style["o" + property] = value;
+		element.style[property] = value;
+		element.style.top = 'calc(50% - ' + maxH / 2 + 'px)';
+		element.style.left = 'calc(50% - ' + maxW / 2 + 'px)';
+	}
+	setVendor(game, 'Transform', scale)
+
 }
-
-$(window).resize(function(){
-	return zoomDiv();
-})
-
-$(document).ready(function() {
-	return zoomDiv();
-});
